3281ee6bb456f113031f0e29ca93a323e46d54df
Gabriel Alves Do Carmo <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2025-08-29 15:34:55 -0300

    chore(first-request): first request test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+let id = 0
+let usuarios : {id: number, name: string, email: string}[] = []
 
 app.get("/", (req: express.Request, res: express.Response) => {
-    res.status(200).send("Hello World!");
+    res.status(200).send("Hello ----- World!");
+})
+
+app.get("/users", (req: express.Request, res: express.Response) => {
+    res.status(200).send(usuarios);
+})
+
+app.get("/users/:id",(req: express.Request, res: express.Response) => {
+    let userID = Number(req.params.id)
+    let user = usuarios.find(user => user.id == userID )
+    res.send(user);
+})
+
+app.post("/users", (req: express.Request, res: express.Response) => {
+    let reqUsers = req.body
+    reqUsers.id = ++id;
+    usuarios.push(reqUsers)
+    res.send({
+        message: "Usuario adicionado com sucesso"
+    })
 })
 
 app.listen(3000, () => console.log("Server started on port 3000"));
